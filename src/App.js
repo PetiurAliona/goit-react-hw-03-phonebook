dca4c78d58cdbfc488ba53aefd07bb42437041c3
PetiurAliona <alena.petyur@gmail.com>
2021-08-28 18:40:14 +0300

    + cleared Arr of contacts

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import ContactsList from "./components/ContactsList/ContactsList"
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   }
-  
+
   componentDidMount() {
     const contacts = localStorage.getItem("contacts")
     const parseCont = JSON.parse(contacts)
@@ -56,8 +51,6 @@ class App extends Component {
     const normalizedFilter = this.state.filter.toLowerCase()
     return this.state.contacts.filter((contact) => contact.name.toLowerCase().includes(normalizedFilter))
   }
-
-
 
   render() {
     const { filter } = this.state
